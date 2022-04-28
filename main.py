f60d88e31bf9dc1ba7090f4a11b2871b5aac0d79
Muhammed Samy <61308233+Mohammed9477@users.noreply.github.com>
2022-04-28 06:05:32 +0200

    the first day commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import csv
+file = open(r"C:\Users\moham\Downloads\Emissions.csv")
+mydict = {}
+reader = csv.reader(open(r"C:\Users\moham\Downloads\Emissions.csv"))
+for i, rows in enumerate(reader):
+        if i == 0: continue
+        k = rows[0]
+        v = rows[1]
+        if not k in mydict:
+            mydict[k] = [v]
+        else:
+            mydict[k].append(v)
+print (mydict)
+print("the first day of the project :")
